68b2d660ffbb8eb1ac677e611e19506778c9a504
Vyacheslav <avalsoff@gmail.com>
2018-08-29 17:35:59 +1000

    Add news page

--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,29 @@ $(() => {
     slidesToShow: 2,
     prevArrow: '<button type="button" class="slide__arrow slide__arrow--prev">Previous</button>',
     nextArrow: '<button type="button" class="slide__arrow slide__arrow--next">Next</button>',
-  });
+  })
+
+  const body = $('body')
+  let i = 0
+  let isMagicStarted = false;
+
+  const magic = () => {
+    body.css('filter', `blur(${i}px)`);
+    i += .8;
+  }
+
+  let interval = {};
+
+  const startMagic = () => {
+    if (!isMagicStarted) {
+      isMagicStarted = true;
+      interval = setInterval(magic, 100)
+    } else {
+      isMagicStarted = false;
+      clearInterval(interval)
+    }
+  }
+
+  $('.logo').click(startMagic)
+
 })
