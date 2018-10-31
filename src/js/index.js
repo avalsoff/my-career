69cdd0f7c93e0060b2e6d59951272c82939e4e99
vyacheslav <vyacheslav@media500.com>
2018-10-31 16:10:50 +1000

    Add chart-group block

--- src/js/index.js
+++ src/js/index.js
@@ -1,123 +1,130 @@
 'use strict';
-(function ($) {
-  function slickArrow(mod) {
-    return `<button type="button" class="slick__arrow slick__arrow--${mod}">Previous</button>`
-  }
+let $ = jQuery;
 
-  function makeSlickCarousel(selector, slidesToShow) {
-    $(selector).slick({
-      infinite: true,
-      slidesToShow,
-      prevArrow: slickArrow('prev'),
-      nextArrow: slickArrow('next'),
-      responsive: [
-        {
-          breakpoint: 992,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        },
-      ]
-    });
-  }
+function slickArrow(mod) {
+  return `<button type="button" class="slick__arrow slick__arrow--${mod}">Previous</button>`
+}
 
-  function makeChart(jqObj, type, categories, data) {
-    new Highcharts.Chart({
-      title: '',
-      tooltip: {
-        enabled: false
-      },
-      chart: {
-        type,
-        renderTo: jqObj[0]
-      },
-      xAxis: {
-        categories
-      },
-      yAxis: {
-        title: false
-      },
-      series: [{
-        data,
-        showInLegend: false,
-        title: '',
-        color: '#0070BA',
-        pointWidth: 20,
-        dataLabels: {
-          enabled: true
-        }
-      }]
-    });
-  }
-
-  function initCharts(selector) {
-    let $charts = $(selector);
-    $charts.each(function (i, el) {
-      let $el = $(el);
-      let { type, categories, data } = $el.data('chart');
-      makeChart($el, type, categories, data);
-    });
-  }
-
-  function tabFactory($buttons, $prevActive) {
-    let $currentActive = null;
-
-    return () => {
-      $buttons.click(function () {
-        if ($currentActive) {
-          $prevActive = $currentActive;
-        }
-        const currentSelector = '.' + this.classList[0];
-        $currentActive = $(currentSelector);
-        if ($prevActive) {
-          $prevActive.toggleClass('active');
-        }
-        $currentActive.toggleClass('active');
-      });
-    }
-  }
-
-  // https://codepen.io/anon/pen/RYYzrq generate a tooltip data attr for the map regions
-
-  $(() => {
-    makeSlickCarousel('.index-page__carousel', 2);
-    makeSlickCarousel('.edu-item-page__carousel', 1);
-    initCharts('.js-chart');
-
-    $('.menu__link--hamburger').click(() => {
-      $('.menu').toggleClass('menu--js-show');
-    });
+function makeSlickCarousel(selector, slidesToShow) {
+  $(selector).slick({
+    infinite: true,
+    slidesToShow,
+    prevArrow: slickArrow('prev'),
+    nextArrow: slickArrow('next'),
+    responsive: [{
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }]
   });
+}
 
-  //Pages
-  function economyPage() {
-    $(() => {
-      const $path = $('.reg-map');
-      $path.hover(function () {
-        $infoBox.css('display', 'block');
-        $infoBox.html($(this).data('info'));
-      });
+function makeChart(jqObj, type, categories, data) {
+  new Highcharts.Chart({
+    title: '',
+    tooltip: {
+      enabled: false
+    },
+    chart: {
+      type,
+      renderTo: jqObj[0],
+      width: 550,
+      height: 80 + '%'
+    },
+    xAxis: {
+      categories
+    },
+    yAxis: {
+      title: false
+    },
+    series: [{
+      data,
+      showInLegend: false,
+      title: '',
+      color: '#0070BA',
+      pointWidth: 20,
+      dataLabels: {
+        enabled: true
+      }
+    }]
+  });
+}
 
-      $path.mouseleave(e => {
-        $infoBox.css('display', 'none');
-      });
+function initCharts(selector) {
+  let $charts = $(selector);
+  $charts.each(function (i, el) {
+    let $el = $(el);
+    let {
+      type,
+      categories,
+      data
+    } = $el.data('chart');
+    makeChart($el, type, categories, data);
+  });
+}
 
-      const $infoBox = $('#info-box');
-      $(document).mousemove(e => {
-        $infoBox.css('top', e.pageY - $infoBox.height() - 30);
-        $infoBox.css('left', e.pageX - $infoBox.width() / 2);
-      }).mouseover();
+function tabFactory($buttons, $prevActive) {
+  let $currentActive = null;
 
-      tabFactory(
-        $('.js-map-tab-button'),
-        $('.js-map-tab1'),
-      )();
-      tabFactory(
-        $('.js-section-tab-button'),
-        $('.js-section-tab1'),
-      )();
-
+  return () => {
+    $buttons.click(function () {
+      if ($currentActive) {
+        $prevActive = $currentActive;
+      }
+      const currentSelector = '.' + this.classList[0];
+      $currentActive = $(currentSelector);
+      if ($prevActive) {
+        $prevActive.toggleClass('active');
+      }
+      $currentActive.toggleClass('active');
     });
   }
-})(jQuery);
+}
+
+// https://codepen.io/anon/pen/RYYzrq generate a tooltip data attr for the map regions
+
+$(() => {
+  makeSlickCarousel('.index-page__carousel', 2);
+  makeSlickCarousel('.edu-item-page__carousel', 1);
+  initCharts('.js-chart');
+
+  $('.menu__link--hamburger').click(() => {
+    $('.menu').toggleClass('menu--js-show');
+  });
+});
+
+//Pages
+function economyPage() {
+  $(() => {
+    const $path = $('.reg-map');
+    $path.hover(function () {
+      $infoBox.css('display', 'block');
+      $infoBox.html($(this).data('info'));
+    });
+
+    $path.mouseleave(e => {
+      $infoBox.css('display', 'none');
+    });
+
+    const $infoBox = $('#info-box');
+    $(document).mousemove(e => {
+      $infoBox.css('top', e.pageY - $infoBox.height() - 30);
+      $infoBox.css('left', e.pageX - $infoBox.width() / 2);
+    }).mouseover();
+
+    tabFactory(
+      $('.js-map-tab-button'),
+      $('.js-map-tab1'),
+    )();
+    tabFactory(
+      $('.js-section-tab-button'),
+      $('.js-section-tab1'),
+    )();
+    tabFactory(
+      $('.js-chartgroup-tab-button'),
+      $('.js-chartgroup-tab1'),
+    )();
+  });
+}
